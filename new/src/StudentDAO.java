e9a26b71ba42a2b89b3b3f37dac703bcabc14bd1
南洋 <100465200+nany9999@users.noreply.github.com>
2022-04-05 13:11:48 +0800

    Delete new directory

--- new/src/StudentDAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface StudentDAO{
-    public void insert(Student s);
-    public void update(Student s);
-    public void delete(long iD);
-    public Student findByID(long iD);
-    public List<Student> findAll();
-} 
